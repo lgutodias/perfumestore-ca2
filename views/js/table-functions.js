6499d042de92c1ff929fc2c1f8206d461112211b
lgutodias <dias.luizaugusto@gmail.com>
2021-04-23 17:51:32 +0000

    css edited

--- views/js/table-functions.js
+++ views/js/table-functions.js
@@ -1,3 +1,21 @@
+function calculateBill() {
+    var fBillTotal = 0.0;
+    var i = 0;
+    var aCBTags = document.querySelectorAll('input');
+    for (i = 0; i < aCBTags.length; i++) {
+        // is this menu item selected? it is if the checkbox is checked
+        if (aCBTags[i].checked) {
+            // get the checkbox' parent table row
+            var oTR = getParentTag(aCBTags[i], 'TR');
+            // retrieve the price from the price column, which is the third column in the table
+            var oTDPrice = oTR.getElementsByTagName('TD')[4];
+            // the first child text node of the column contains the price
+            fBillTotal += parseFloat(oTDPrice.firstChild.data);
+        };
+    };
+    // return the price as a decimal number with 2 decimal places
+    return Math.round(fBillTotal * 100.0) / 100.0;
+};
 
 function highlightSale(idTable, bShowSale) {
     // if bShowSale is true, then we're highlighting vegetarian
@@ -17,4 +35,15 @@ function highlightSale(idTable, bShowSale) {
             };
         };
     };
+};
+// Utility function for getting the parent tag of a given tag
+// but only of a certain type (i.e. a TR, a TABLE, etc.)
+function getParentTag(oNode, sParentType) {
+    var oParent = oNode.parentNode;
+    while (oParent) {
+        if (oParent.nodeName == sParentType)
+            return oParent;
+        oParent = oParent.parentNode;
+    };
+    return oParent;
 };
